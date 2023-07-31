85863051b8ea2bf658d1459cb812c611dc7cee7e
david-creator-app <141047321+david-creator-app@users.noreply.github.com>
2023-08-01 01:14:59 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="SPORT UNIVERSE" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          All Games You Want To Watch!
         </p>
       </main>
 
